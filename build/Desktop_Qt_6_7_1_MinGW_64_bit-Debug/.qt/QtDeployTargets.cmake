061b70aecd44906c6b2507c1f9f9cfc7e254ff5b
potakaaa <helbirog@gmail.com>
2024-05-15 20:08:21 +0800

    Added new files

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_test_FILE D:/Documents/PROGRAMMING/C++/Qt 6.0/test/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/test.exe)
+set(__QT_DEPLOY_TARGET_test_RUNTIME_DLLS D:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
